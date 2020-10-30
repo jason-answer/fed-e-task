0e0ed1487c2c091c9e589a898a90d6ce91b242de
ChenGang <gang.a.chen@capgemini.com>
2020-10-30 16:54:40 +0800

    docs: ✏️ Add leaning code and handouts of  03-04/05/06

---
+++ learning-materials/codes/03-06-codes/edu-boss-fed-master/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>拉勾教育</h1>
+    <!-- 根路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped></style>
